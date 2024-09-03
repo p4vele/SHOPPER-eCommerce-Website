98c320fdd5324179e12657ad9c4adfebbc45550d
p4vele <versiontv1@gmail.com>
2024-09-04 02:39:45 +0300

    ProductDisplay Front

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId}= useParams();
+  const product = all_product.find(item => item.id === Number(productId));
+  
   return (
-    <div>
-        
+    <div className='product'>
+        <Breadcrum product={product}/>
+        <ProductDisplay product={product}/>
+
     </div>
   )
 }
