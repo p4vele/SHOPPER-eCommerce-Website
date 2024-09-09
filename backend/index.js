f329a7a1dde742b06970138cac7ce4326ffc8f36
p4vele <versiontv1@gmail.com>
2024-09-09 13:07:02 +0300

    newcollection and popular items connected to db

--- backend/index.js
+++ backend/index.js
@@ -228,3 +228,20 @@ app.post('/login',async(req, res)=>{
         }
     }
 })
+
+//creating endpoint for newcollection data 
+app.get('/newcollections',async(req, res)=>{
+    let products =await Product.find({});
+    let newcollection = products.slice(1).slice(-8);
+    console.log("new collection fetched");
+    res.send(newcollection);
+})
+
+//creating endpoint for popular in women section
+app.get('/popularinwomen',async(req, res)=>{
+    let products =await Product.find({category: "women"});
+    let popular_in_women = products.slice(0.4);
+    console.log("popular_in_women fetched");
+    res.send(popular_in_women);
+})
+
