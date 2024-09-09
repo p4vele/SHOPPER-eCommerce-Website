8421140297b6573a48caa1a0265639e58f7466e0
p4vele <versiontv1@gmail.com>
2024-09-09 18:41:14 +0300

    db product images

--- backend/index.js
+++ backend/index.js
@@ -240,7 +240,7 @@ app.get('/newcollections',async(req, res)=>{
 //creating endpoint for popular in women section
 app.get('/popularinwomen',async(req, res)=>{
     let products =await Product.find({category: "women"});
-    let popular_in_women = products.slice(0.4);
+    let popular_in_women = products.slice(0,4);
     console.log("popular_in_women fetched");
     res.send(popular_in_women);
 })
