4626670e8a4b1762a366f48b6621ea0a4fcc4d75
p4vele <versiontv1@gmail.com>
2024-09-10 16:50:54 +0300

    ShopCategory sorting by price functunality

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './CSS/ShopCaregory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
@@ -8,28 +8,53 @@ const ShopCategory = (props) => {
 
   const {all_product}=useContext(ShopContext);
 
+  const [sortBy, setSortBy] = useState('default'); 
+
+  const sortProducts = (products) => {
+    if (sortBy === 'price-asc') {
+      return [...products].sort((a, b) => a.new_price - b.new_price);
+    } else if (sortBy === 'price-desc') {
+      return [...products].sort((a, b) => b.new_price - a.new_price);
+    }
+    return products;
+  };
+
+  const filteredProducts = all_product.filter((item) => item.category === props.category);
+  const sortedProducts = sortProducts(filteredProducts);
+
   return (
     <div className='shop-category'>
         <img className='shopcategory-banner' src={props.banner} alt="" />
         <div className="shopcategory-indexSort">
           <p>
-            <span>Showing 1-12</span> out of 36 products
+            <span>Showing 1-12</span> out of {filteredProducts.length} products
           </p>
           <div className="shopcategory-sort">
-            sort by <img src={dropdown_icon} alt="" />
+            Sort by
+            <select 
+              value={sortBy} 
+              onChange={(e) => setSortBy(e.target.value)}
+              className="sort-dropdown"
+            >
+              <option value="default">Default</option>
+              <option value="price-asc">Price: Low to High</option>
+              <option value="price-desc">Price: High to Low</option>
+            </select>
           </div>
         </div>
         <div className="shopcategory-products">
-          {all_product.map((item,i) => {
-            if (props.category === item.category){
-              return <Item  key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            }
-            else{
-              return null;
-            }
-          })}
+          {sortedProducts.slice(0, 12).map((item, i) => (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          ))}
         </div>
-        <div className="shopcategory-loadmore">
+        <div className="shopcategory-loadmore" onClick={()=>{window.scrollTo(0, 0);}}>
           Explore More
         </div>
     </div>
